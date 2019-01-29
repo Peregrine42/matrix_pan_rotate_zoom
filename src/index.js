c37e8285e8888961523ed176398e9b646bdfdae6
Duncan Fermor <duncan_fermor@fastmail.com>
2019-01-29 01:58:28 +0000

    Matrix transforms in place (I think).

--- src/index.js
+++ src/index.js
@@ -1,10 +1,47 @@
 import waitForDOM from "./utils/waitForDOM"
-import {scale, rotate, translate, compose, applyToPoint} from 'transformation-matrix';
+import {scale, rotateDEG, translate, compose} from 'transformation-matrix';
+import SVG from 'svg.js'
+import { Tile } from './model/tile'
 
 let props = {}
 
 const main = (props) => {
-    console.log("Hi!")
+    let tiles = [
+        new Tile(100, 0, "/images/card1.png", "card1", true, 0, 1),
+    ]
+    let visibleImageTiles = tiles.filter(t => t.visible && t.url)
+    let map1 = SVG("map1")
+
+    let images = visibleImageTiles.map((tile) => { 
+        let element = map1
+            .image(tile.url)
+            .hide()
+            .loaded(function(loader) {
+                this.size(loader.width, loader.height)
+                this.show()
+
+                tile.width = loader.width
+                tile.height = loader.height
+            })
+        tile.element = element
+    })
+
+    setTimeout(() => {
+        let x = map1.width()/2
+        let y = map1.height()/2
+        let rotation = 90
+
+        visibleImageTiles.forEach((tile) => { 
+            let m = compose(
+                rotateDEG(rotation, map1.width()/2, map1.height()/2),
+                translate(x,y),
+                translate(tile.x, tile.y),
+                translate(-tile.width/2, -tile.height/2),
+                rotateDEG(tile.rotation, tile.width/2, tile.height/2),
+            )
+            tile.element.transform(m)
+        })
+    }, 2000)
 }
 
 waitForDOM()
---
+++ src/model/tile.js
@@ -0,0 +1,16 @@
+import {applyToPoint} from 'transformation-matrix';
+
+export class Tile {
+    constructor(x = 0, y = 0, url = false, group = false, visible = false, rotation = 0, scale = 1.0) {
+        this.x = x
+        this.y = y
+
+        this.rotation = rotation
+        this.scale = scale
+
+        this.url = url
+        this.visible = visible
+
+        this.group = group
+    }
+}
