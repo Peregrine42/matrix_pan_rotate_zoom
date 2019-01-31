485ba1628e9dea2e167f5838f88d4489f798825e
Duncan Fermor <duncan_fermor@fastmail.com>
2019-01-31 21:34:54 +0000

    Major refactor using bind to reduce global state.

--- src/index.js
+++ src/index.js
@@ -4,25 +4,43 @@ import Tile from './model/tile'
 import buildTransform from './utils/geometry/buildTransform'
 import normalize from './utils/geometry/normalize'
 
-let props = {}
+function keyHandler(down, e) {
+    if (e.key === "ArrowUp") {
+        this.up = down
+    } else if (e.key === "ArrowDown") {
+        this.down = down
+    } else if (e.key === "ArrowLeft") {
+        this.left = down
+    } else if (e.key === "ArrowRight") {
+        this.right = down
+    } else if (e.key === "=") {
+        this.zoomin = down
+    } else if (e.key === "-") {
+        this.zoomout = down
+    } else if (e.key === "[") {
+        this.rotateleft = down
+    } else if (e.key === "]") {
+        this.rotateright = down
+    }
+}
 
-const update = (tiles, map, screenX, screenY, x, y, rotation, zoom) => {
-    tiles.forEach((tile) => {
+function update() {
+    this.tiles.forEach(function(tile) {
         let m = buildTransform(
-            screenX, screenY,
-            x, y,
-            rotation,
-            zoom,
+            this.screenX, this.screenY,
+            this.x, this.y,
+            this.rotation,
+            this.zoom,
             tile.x, tile.y,
             tile.rotation,
             tile.scale,
             tile.width, tile.height
         )
         tile.element.transform(m)
-    })
+    }.bind(this))
 }
 
-const clamp = (val, bound) => {
+function clamp(val, bound) {
     if (val > bound) {
         val = bound
     } else if (val < -bound) {
@@ -31,22 +49,20 @@ const clamp = (val, bound) => {
     return val
 }
 
-const main = (props) => {
+function main() {
     let tiles = [
         new Tile(0, 0, "/images/card1.png", "card1", true, 0, 0.25),
         new Tile(100, 0, "/images/card1.png", "card1", true, 0, 0.25),
         new Tile(-100, 0, "/images/card1.png", "card1", true, 90, 0.25),
     ]
-    let visibleImageTiles = tiles.filter(t => t.visible && t.url)
     let map1 = SVG("map1")
 
-    let images = visibleImageTiles.map((tile) => {
+    let images = tiles.map((tile) => {
         let element = map1
             .image(tile.url)
             .hide()
             .loaded(function(loader) {
                 this.size(loader.width, loader.height)
-                this.show()
 
                 tile.width = loader.width
                 tile.height = loader.height
@@ -54,129 +70,100 @@ const main = (props) => {
         tile.element = element
     })
 
-    setTimeout(() => {
-        let width = map1.width()
-        let height = map1.height()
-        let screenX = width / 2
-        let screenY = height / 2
-        let x = 0
-        let y = 0
-        let rotation = 0
-        let zoom = 0.5
+    let state = {
+        width: map1.width(),
+        height: map1.height(),
+        screenX: 0,
+        screenY: 0,
+        x: 0,
+        y: 0,
+        rotation: 0,
+        zoom: 0.5,
 
-        let up = false
-        let down = false
-        let left = false
-        let right = false
+        up: false,
+        down: false,
+        left: false,
+        right: false,
 
-        let zoomin = false
-        let zoomout = false
+        zoomin: false,
+        zoomout: false,
 
-        let rotateleft = false
-        let rotateright = false
+        rotateleft: false,
+        rotateright: false,
 
-        update(tiles, map1, screenX, screenY, x, y, rotation, zoom)
+        stride: 5,
+        zoomFactor: 0.950,
+        tiles: tiles
+    }
 
-        const step = () => {
-            let dirty = false
-            let stride = 5
-            let zoomFactor = 0.950
+    state.screenX = state.width / 2
+    state.screenY = state.height / 2
 
-            if (up) {
-                const v = normalize(0, -stride, rotation, zoom)
-                x += v.x
-                y += v.y
-                dirty = true
-            }
-            if (down) {
-                const v = normalize(0, stride, rotation, zoom)
-                x += v.x
-                y += v.y
-                dirty = true
-            }
-            if (left) {
-                const v = normalize(-stride, 0, rotation, zoom)
-                x += v.x
-                y += v.y
-                dirty = true
-            }
-            if (right) {
-                const v = normalize(stride, 0, rotation, zoom)
-                x += v.x
-                y += v.y
-                dirty = true
-            }
-            if (zoomin) {
-                zoom = zoom * (2-zoomFactor)
-                dirty = true
-            }
-            if (zoomout) {
-                zoom = zoom * zoomFactor
-                dirty = true
-            }
-            if (rotateleft) {
-                rotation -= 3
-                dirty = true
-            }
-            if (rotateright) {
-                rotation += 3
-                dirty = true
-            }
+    window.addEventListener("keyup", keyHandler.bind(state, false))
 
-            if (dirty) {
-                x = clamp(x, width)
-                y = clamp(y, height)
-                update(tiles, map1, screenX, screenY, x, y, rotation, zoom)
-            }
+    window.addEventListener("keydown", keyHandler.bind(state, true))
 
-            window.requestAnimationFrame(step)
-        }
-
-        window.requestAnimationFrame(step)
-
-        window.addEventListener("keyup", (e) => {
-            if (e.key === "ArrowUp") {
-                up = false
-            } else if (e.key === "ArrowDown") {
-                down = false
-            } else if (e.key === "ArrowLeft") {
-                left = false
-            } else if (e.key === "ArrowRight") {
-                right = false
-            } else if (e.key === "=") {
-                zoomin = false
-            } else if (e.key === "-") {
-                zoomout = false
-            } else if (e.key === "[") {
-                rotateleft = false
-            } else if (e.key === "]") {
-                rotateright = false
-            }
-        })
-
-        window.addEventListener("keydown", (e) => {
-            if (e.key === "ArrowUp") {
-                up = true
-            } else if (e.key === "ArrowDown") {
-                down = true
-            } else if (e.key === "ArrowLeft") {
-                left = true
-            } else if (e.key === "ArrowRight") {
-                right = true
-            } else if (e.key === "=") {
-                zoomin = true
-            } else if (e.key === "-") {
-                zoomout = true
-            } else if (e.key === "[") {
-                rotateleft = true
-            } else if (e.key === "]") {
-                rotateright = true
-            }
-        })
+    window.setTimeout(() => {
+        tiles.forEach((tile) => { tile.element.show() })
+        update.bind(state)()
     }, 2000)
+    window.requestAnimationFrame(step.bind(state))
+}
+
+function step() {
+    let dirty = false
+
+    if (this.up) {
+        const v = normalize(0, -this.stride, this.rotation, this.zoom)
+        this.x += v.x
+        this.y += v.y
+        dirty = true
+    }
+    if (this.down) {
+        const v = normalize(0, this.stride, this.rotation, this.zoom)
+        this.x += v.x
+        this.y += v.y
+        dirty = true
+    }
+    if (this.left) {
+        const v = normalize(-this.stride, 0, this.rotation, this.zoom)
+        this.x += v.x
+        this.y += v.y
+        dirty = true
+    }
+    if (this.right) {
+        const v = normalize(this.stride, 0, this.rotation, this.zoom)
+        this.x += v.x
+        this.y += v.y
+        dirty = true
+    }
+    if (this.zoomin) {
+        this.zoom = this.zoom * (2 - this.zoomFactor)
+        dirty = true
+    }
+    if (this.zoomout) {
+        this.zoom = this.zoom * this.zoomFactor
+        dirty = true
+    }
+    if (this.rotateleft) {
+        this.rotation -= 3
+        dirty = true
+    }
+    if (this.rotateright) {
+        this.rotation += 3
+        dirty = true
+    }
+
+    if (dirty) {
+        this.x = clamp(this.x, this.width)
+        this.y = clamp(this.y, this.height)
+        update.bind(this)()
+    }
+
+    window.requestAnimationFrame(step.bind(this))
 }
 
 waitForDOM()
     .then(() => {
-        main(props)
+        main()
     })
